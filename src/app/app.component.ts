bb4de74dcc2bd5234b2f8b71b013b674dd7af238
Alex <off@tut.by>
2021-06-11 10:14:20 +0300

    Cleanup readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div class="red">
+      <h1 [innerHTML]='title'> </h1>
+      <h1>{{title}}</h1>
+      <input type="text" [value]='name'>
+      <div>
+
+      </div>
+    </div>
+  `,
+  //templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ultcourse';
+  title: string = 'Ultimate Angular';
+  logo: string = "../img/logo.jpg"
+  name: string = 'Sasha'
+
+
+  constructor() {
+    
+  }
+
 }
