f31b1867f6ae7707845b9e7d1ceaf6a31fa2e5d5
Alex <off@tut.by>
2021-06-17 15:14:11 +0300

    *ngIf, ng-tempalte

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,20 +4,27 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <div class="mx-5 my-5">
-      <button class="btn btn-primary" (click)="handleClick(username.value)">Get Value</button>
-      <h1 [innerHTML]='title'> </h1>
+     <!--  <button class="btn btn-primary"  (click)="handleClick(username.value)">Get Value</button> -->
+<!--       <h1 [innerHTML]='title'> </h1> -->
       <h1>{{title}}</h1>
       <input 
         type="text"
         ([ngModel])="name"
-        ><!-- (ngModelChange)="handleChange($event)" -->
-      >
-      <input type="text" #username>
+        (input)='handleChange($event.target.value)'
+        >
+        <!-- (ngModelChange)="handleChange($event)" -->
+        <!--    <input type="text" #username> -->
         <div>
-      <img [src]="logo" alt="">
+        <!--       <img [src]="logo" alt=""> -->
       </div>
+
+      <ng-template [ngIf]="name.length > 4">
       <div>
-        {{name}}
+       Searching for... {{name}}
+      </div>
+      </ng-template>
+       <div *ngIf="name.length > 4">
+       Searching for... {{name}}
       </div>
     </div>
   `,
@@ -25,14 +32,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title: string = 'Ultimate Angular';
+  title: string = '';
   logo: string = "https://blog.whoishiring.io/content/images/2017/03/todd_us.png"
-  name: string = 'Sasha'
+  name: string = '';
 
-  handleClick(value: string) {
-    console.log(value);
-    //this.name = 'Sashiko';
-  }
+
+   
+  handleChange(value: string) {
+    this.name = value;
+  } 
 
   constructor() {
     
@@ -45,9 +53,10 @@ export class AppComponent {
   handleInput(event: any) {
     this.name = event.target.value;
   } */
- 
-  /* handleChange(value: string) {
-    this.name = value
-  } */
 
+/*   
+  handleClick(value: string) {
+    console.log(value);
+    this.name = 'Sashiko';
+  } */
 }
