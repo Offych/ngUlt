cc0ca9ff0e15d6bd5252e2b25dc7ab3672cd2f18
Alex <off@tut.by>
2021-06-25 10:25:00 +0300

    Routing module added and start usage it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,26 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
     FormsModule,
+    RouterModule.forRoot(routes),
     PassengerDashboardModule
   ],
   providers: [],
